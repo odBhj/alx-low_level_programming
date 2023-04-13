80fb241b56e0741ee93cd75c235f6249c36ebbeb
Mohammed Bahaj <odbhj51@gmail.com>
2023-04-13 09:18:25 -0700

    1. string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,26 +1,27 @@
-#include <stdlib.h>		
-#include "main.h"		
-/**		
+#include <stdlib.h>
+#include "main.h"
+/**
  * *string_nconcat - concatenates two strings s1 and v2
  * @v1: string
  * @v2: string
- * @n: number of bytes from v2 to concatenate to v1		
- * Return: pointer to the resulting string	
+ * @n: number of bytes from v2 to concatenate to v1
+ * Return: pointer to the resulting string
  */
-char *string_nconcat(char *s1, char *s2, unsigned int n) {
+char *string_nconcat(char *v1, char *v2, unsigned int n)
+{
 unsigned int len1 = 0, len2 = 0, i, j;
 char *result;
-if (s1 == NULL)
+if (v1 == NULL)
 {
-s1 = "";
+v1 = "";
 }
-if (s2 == NULL)
+if (v2 == NULL)
 {
-s2 = "";
+v2 = "";
 }
-while (s1[len1])
+while (v1[len1])
 len1++;
-while (s2[len2])
+while (v2[len2])
 len2++;
 if (n >= len2)
 {
@@ -29,12 +30,12 @@ n = len2;
 result = malloc((len1 + n + 1) * sizeof(char));
 if (result == NULL)
 {
-return NULL;
+return (NULL);
 }
-for (i = 0; s1[i]; i++)
-result[i] = s1[i];
+for (i = 0; v1[i]; i++)
+result[i] = v1[i];
 for (j = 0; j < n; j++)
-result[i + j] = s2[j];
+result[i + j] = v2[j];
 result[i + j] = '\0';
-return result;
+return (result);
 }
