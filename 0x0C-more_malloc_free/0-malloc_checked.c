15a79220b209591550cabd225f073ec1542488db
Mohammed Bahaj <odbhj51@gmail.com>
2023-04-13 08:40:11 -0700

    Trust no one

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+* *malloc_checked -  allocates memory using malloc.
+* @b: allocated bytes
+* Return: the pointer
+*/
+void *malloc_checked(unsigned int b)
+{
+void *ptr;
+ptr = malloc(b);
+if (ptr == NULL)
+{
+exit(98);
+}
+return (ptr);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+C - 0x0C-more_malloc_free
