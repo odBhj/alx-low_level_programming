9cbdd5fee4d03a01e2262201d6045efd92bb0ba3
Mohammed Bahaj <odbhj51@gmail.com>
2023-04-18 18:13:54 -0700

    4. A door is what a dog is perpetually on the wrong side of

---
+++ 0x0E-structures_typedef/strlen.c
@@ -0,0 +1,16 @@
+/**
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+    int len = 0;
+    while (*s != '\0') {
+        len++;
+        s++;
+    }
+    return len;
+}
+
