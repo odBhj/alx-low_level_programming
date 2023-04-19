c58ff40ddc903cbe8e75afd8c63cc2a54ba0e216
Mohammed Bahaj <odbhj51@gmail.com>
2023-04-19 13:16:13 -0700

    hot fix

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,16 @@
-#include <stdio.h>
 #include "function_pointers.h"
+#include <stdio.h>
 /**
  * print_name - prints a name as is
  * @name: name of the person
  *@f: how to print the name
- * Return: Null if error, char is success.
+ * Return: Null
  */
 void print_name(char *name, void (*f)(char *))
 {
+if(f == NULL || name == NULL)
+{
+return;
+}
 f(name);
 }
