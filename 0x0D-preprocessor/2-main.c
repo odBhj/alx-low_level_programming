1373eeecc84602f21406e2eb08805c09d0b875a3
Mohammed Bahaj <odbhj51@gmail.com>
2023-04-18 18:42:16 -0700

    file name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints the name of the file
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *file = __FILE__;
+	printf("%s\n", file);
+	return 0;
+}
